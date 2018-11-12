76d7eb5a83c6af10238f24b9e28291839bd2c71e
Linus S <LinusS1@users.norelply.github.com>
2018-11-11 18:26:47 -0800

    Added utterance recognizion
    next step:
    Get install working!

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 from mycroft.skills.core import FallbackSkill
-from mycroft.filesystem import FileSystemAccess
 from mycroft.util import normalize
+from msm import 
+import string
 import requests
+import json
 
 #The url to get the data
 url = "https://raw.githubusercontent.com/MycroftAI/mycroft-skills-data/18.08/skill-metadata.json"
@@ -17,48 +19,30 @@ class SkillRecommendationsFallback(FallbackSkill):
         """Register and download the file"""
         self.register_fallback(self.handle_fallback, 17) # Q:why 17? A:Why not?
         #Download
-        r = requests.get(url, allow_redirects=True)
-        self.write_file_bytes("skills_data.json", r.content)
-        
-        #TODO: Do we really need to write this to a file?
-        self.log.info("Loading examples for fallback")
-        data = self.read_file("skills_data.json")
-        data = json.loads(self.data)
+        data = requests.get(url, allow_redirects=True).json()
         
         self.examples_list = []
         
-        for key, value in self.examples_list.items():
+        for key, value in data.items():
             self.examples_list.append({"skill":key, "examples":value.get("examples")})
     
-    def read_file(self, filename):
-        fs = FileSystemAccess(str(self.skill_id))
-        data_file = fs.open(filename, "r")
-        data = data_file.read()
-        data_file.close()
-        return data
-
-    def write_file_bytes(self, filename, data):
-        fs = FileSystemAccess(str(self.skill_id))
-        data_file = open(filename, 'wb')
-        data_file.write(data)
-        data_file.close()
-        return True
-    
-    def skill_search(utter):
+    def skill_search(self, utter):
         for idx, dic in enumerate(self.examples_list):
             for idx, example in enumerate(dic.get("examples")):
-                if example.normalize(remove_articles=True) == utter.normalize(remove_articles=True):
+                if normalize(example, remove_articles=True).lower().translate({ord(c): None for c in string.punctuation}) == normalize(utter, remove_articles=True).lower().translate({ord(c): None for c in string.punctuation}):
                     return str(dic.get("skill"))
     
     def handle_fallback(self, message):
         """Find the skill and offer to download it"""
         utter = message.data.get("utterance")
         suggested_skill = self.skill_search(utter)
+        self.log.error(str(suggested_skill))
         if suggested_skill == None:
             return False
         else:
             #We can download the skill
-            pass
+
+            return True
         
         def shutdown(self):
             """Remove skill from list of skills"""
